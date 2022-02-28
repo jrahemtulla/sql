91c9504b20dd1bbd10718503b5f889b26b4e3c32
jrahemtulla <96964093+jrahemtulla@users.noreply.github.com>
2022-02-28 10:54:48 -0500

    Add files via upload

---
+++ queries/00_blank copy.sql
@@ -0,0 +1,13 @@
+-- SELECT Name, (Students*1000) AS Population
+-- FROM Colleges 
+-- WHERE City = 'Cambridge'
+
+-- SELECT FirstName,
+-- 	LastName,
+--     Birthdate,
+--     TIMESTAMPDIFF(YEAR,Birthdate,now())
+-- FROM Students
+
+SELECT *
+FROM Students
+WHERE Phone IS NOT NULL
